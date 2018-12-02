1ddb8a0f5a203a68c9815174688168a572f0b912
root <amaze2333@gmail.com>
2018-12-03 00:38:05 +0800

    changes

--- content/📖books/my-first-post.md
+++ content/📖books/my-first-post.md
@@ -1,5 +1,5 @@
 ---
-title: "第4本"
+title: "第5本"
 date: 2018-12-02T04:44:38+08:00
 Math: true
 Categories: []
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
-
+git status
+git add .
+git commit -m "changes"
+git push origin master
+hugo -d ../Blackcat1997.github.io/
 cd ../Blackcat1997.github.io/
+git add .
+git status
+git commit -m "updates"
+git push origin master
